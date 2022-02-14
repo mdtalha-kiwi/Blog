c137f7efbd9a0d6b997ea488c0f51ee0d8d764c7
MDTalha178 <talhaazmi178@gmail.com>
2022-02-14 23:35:39 +0530

    home page and static collect

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
 
 def post(request):
     return render(request, 'post/index.html')
